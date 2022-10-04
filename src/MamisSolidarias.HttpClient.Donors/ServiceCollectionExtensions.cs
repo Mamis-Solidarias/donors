e9386f6f77465cc034fb730615d8e982fac9b101
Lucas Dell'Isola <dev@lucasdellisola.com.ar>
2022-10-04 16:21:16 -0300

    [Fix] Added default authorization header

--- src/MamisSolidarias.HttpClient.Donors/ServiceCollectionExtensions.cs
+++ src/MamisSolidarias.HttpClient.Donors/ServiceCollectionExtensions.cs
@@ -1,6 +1,8 @@
 using MamisSolidarias.HttpClient.Donors.Models;
 using MamisSolidarias.HttpClient.Donors.DonorsClient;
+using MamisSolidarias.HttpClient.Donors.Services;
 using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Polly;
@@ -12,24 +14,34 @@ public static class ServiceCollectionExtensions
     /// <summary>
     /// It registers the DonorsHttpClient using dependency injection
     /// </summary>
-    /// <param name="builder"></param>
-    public static void AddDonorsHttpClient(this WebApplicationBuilder builder)
+    /// <param name="services"></param>
+    /// <param name="configuration"></param>
+    public static void AddDonorsHttpClient(this IServiceCollection services, IConfiguration configuration)
     {
-        var configuration = new DonorsConfiguration();
-        builder.Configuration.GetSection("DonorsHttpClient").Bind(configuration);
-        ArgumentNullException.ThrowIfNull(configuration.BaseUrl);
-        ArgumentNullException.ThrowIfNull(configuration.Timeout);
-        ArgumentNullException.ThrowIfNull(configuration.Retries);
+        var config = new DonorsConfiguration();
+        configuration.GetSection("DonorsHttpClient").Bind(config);
+        ArgumentNullException.ThrowIfNull(config.BaseUrl);
+        ArgumentNullException.ThrowIfNull(config.Timeout);
+        ArgumentNullException.ThrowIfNull(config.Retries);
 
-        builder.Services.AddSingleton<IDonorsClient, DonorsClient.DonorsClient>();
-        builder.Services.AddHttpClient("Donors", client =>
+        services.AddHttpContextAccessor();
+        
+        services.AddSingleton<IDonorsClient, DonorsClient.DonorsClient>();
+        services.AddHttpClient("Donors", (services,client) =>
         {
-            client.BaseAddress = new Uri(configuration.BaseUrl);
-            client.Timeout = TimeSpan.FromMilliseconds(configuration.Timeout);
-            client.DefaultRequestHeaders.Add("Content-Type", "application/json");
+            client.BaseAddress = new Uri(config.BaseUrl);
+            client.Timeout = TimeSpan.FromMilliseconds(config.Timeout);
+            
+            var contextAccessor = services.GetService<IHttpContextAccessor>();
+            if (contextAccessor is not null)
+            {
+                var authHeader = new HeaderService(contextAccessor).GetAuthorization();
+                if (authHeader is not null)
+                    client.DefaultRequestHeaders.Add("Authorization", authHeader);
+            }
         })
             .AddTransientHttpErrorPolicy(t =>
-            t.WaitAndRetryAsync(configuration.Retries,
+            t.WaitAndRetryAsync(config.Retries,
                 retryAttempt => TimeSpan.FromMilliseconds(100 * Math.Pow(2, retryAttempt)))
         );
     }

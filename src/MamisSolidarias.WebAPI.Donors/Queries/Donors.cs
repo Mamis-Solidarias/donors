416b67a3ad144b843eabdc55a24db225e2341b5e
Lucas Dell'Isola <dev@lucasdellisola.com.ar>
2022-10-15 13:04:04 -0300

    [Feature] Added filters (#57)

--- src/MamisSolidarias.WebAPI.Donors/Queries/Donors.cs
+++ src/MamisSolidarias.WebAPI.Donors/Queries/Donors.cs
@@ -2,22 +2,41 @@ using HotChocolate.AspNetCore.Authorization;
 using HotChocolate.Types;
 using MamisSolidarias.Infrastructure.Donors;
 using MamisSolidarias.Infrastructure.Donors.Models;
-using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace MamisSolidarias.WebAPI.Donors.Queries;
 
 public class Donors
 {
+    
+    public record DonorFilters(bool? IsGodFather, int? OwnerId, string? Name);
+
     [Authorize(Policy = "CanRead")]
     [UsePaging]
     [UseFiltering]
     [UseSorting]
-    public IQueryable<Donor> GetDonors([FromServices] DonorsDbContext dbContext) =>
-        dbContext.Donors;
+    public IQueryable<Donor> GetDonors(DonorsDbContext dbContext, DonorFilters? filters = null)
+    {
+        var query = dbContext.Donors.AsNoTracking().AsQueryable();
+        if (filters is null)
+            return query;
+
+        if (filters.IsGodFather is not null)
+            query = query.Where(t => t.IsGodFather == filters.IsGodFather);
+        
+        if (filters.OwnerId is not null)
+            query = query.Where(t => t.CreatedBy == filters.OwnerId);
+        
+        if (filters.Name is not null)
+            query = query.Where(t => t.Name.ToLower().Contains(filters.Name.ToLower()));
+
+        return query;
+    }
+       
     
     [Authorize(Policy = "CanRead")]
     [UseFirstOrDefault]
     [UseProjection]
-    public IQueryable<Donor> GetDonor([FromServices] DonorsDbContext dbContext, int? id) =>
+    public IQueryable<Donor> GetDonor(DonorsDbContext dbContext, int? id) =>
         dbContext.Donors.Where(d => d.Id == id);
 }

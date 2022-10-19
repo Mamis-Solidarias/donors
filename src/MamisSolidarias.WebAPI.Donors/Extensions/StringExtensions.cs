a4f0b0e6c2023dd703e2856f611b97d145086512
Lucas Dell'Isola <dev@lucasdellisola.com.ar>
2022-10-19 11:01:52 -0300

    [Fix] Trimmed entries and converted them to null if whitespace or empty

--- src/MamisSolidarias.WebAPI.Donors/Extensions/StringExtensions.cs
+++ src/MamisSolidarias.WebAPI.Donors/Extensions/StringExtensions.cs
@@ -4,8 +4,9 @@ namespace MamisSolidarias.WebAPI.Donors.Extensions;
 
 internal static class StringExtensions
 {
-    public static string PrepareForDb(this string value) => 
-        value.Trim().ToLowerInvariant();
+    public static string? PrepareForDb(this string? value) 
+        => string.IsNullOrWhiteSpace(value) ? null : value.Trim().ToLowerInvariant();
+
 
     public static string Capitalize(this string value) =>
         CultureInfo
